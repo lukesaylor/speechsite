fde8dad57f52428b8ab0118afefdda943e9a1691
Stephanie Altman <slappyhoursound@gmail.com>
2020-11-24 14:23:29 -0500

    navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Navbar.css";
+import Button from "./NavButton";
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+      <div className="Logo">MidAtlanticSpeech</div>
+      <div className="ButtonsContainer">
+        <Button>Home</Button>
+        <Button>Resources</Button>
+        <Button>Find Therapy</Button>
+        <Button>About</Button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
